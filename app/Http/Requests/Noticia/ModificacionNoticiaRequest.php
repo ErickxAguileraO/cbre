a0f25b275514aff004ed3a994c1a971e550db139
Cajorobu <kmilo_rojas_9@hotmail.com>
2023-02-16 14:48:09 -0300

    Modificación noticia
    Se agregó la modificación de la noticia mediante el formulario.

---
+++ app/Http/Requests/Noticia/ModificacionNoticiaRequest.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Requests\Noticia;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ModificacionNoticiaRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'titulo' => [
+                'required',
+                'max:255'
+            ],
+            'imagen' => [
+                'nullable',
+                'mimes:jpg,jpeg,png',
+                'max:5120'
+            ],
+            'cuerpo' => [
+                'required',
+                'max:1000',
+                'min:30'
+            ],
+            'edificio' => [
+                'nullable',
+                'numeric'
+            ]
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'imagen.mimes' => 'Formatos permitidos: jpg, jpeg, png.',
+            'imagen.max' => 'Tamaño de imagen máximo: 5MB',
+        ];
+    }
+}
