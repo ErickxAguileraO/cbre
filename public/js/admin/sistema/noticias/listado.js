32f1aaeb1075de6b6cbceef39b50507d497bb77a
Cajorobu <kmilo_rojas_9@hotmail.com>
2023-02-16 01:04:27 -0300

    Listado noticias
    Se agregó el listado de noticias en el dataGrid de Devextreme.

---
+++ public/js/admin/sistema/noticias/listado.js
@@ -0,0 +1,145 @@
+document.addEventListener('DOMContentLoaded', function () {
+    cargarNoticias();
+
+    function cargarNoticias() {
+        $.ajaxSetup({
+            headers: {
+                'X-CSRF-TOKEN': '{{ csrf_token() }}',
+            },
+        });
+    
+        DevExpress.localization.locale(navigator.language);
+    
+        // Función para el origen de datos.
+        const noticias = new DevExpress.data.CustomStore({ 
+            load: function() {
+                return sendRequest("/admin/noticias/get/list");
+            }
+        });
+    
+        $('#dataGridNoticias').dxDataGrid({
+            dataSource: noticias,
+            columns: [
+                {
+                    dataField: 'not_titulo',
+                    caption: 'Título'
+                },
+                {
+                    dataField: 'nombreEdificio',
+                    caption: 'Edificio',
+                },
+                {
+                    dataField: 'fechaChile',
+                    caption: 'Fecha',
+                    width: '100',
+                    minWidth: '100',
+                },
+                {
+                    dataField: 'hora',
+                    caption: 'Hora',
+                    width: '70',
+                    minWidth: '70',
+                },
+                {
+                    dataField: '',
+                    caption: 'Opciones',
+                    alignment: 'center',
+                    width: '90',
+                    minWidth: '90',
+                    hidingPriority: 4,
+                    cellTemplate(container, options) {
+                        const idCertificacion = options.data.cer_id;
+    
+                        let urlModificar = `/admin/certificaciones/${idCertificacion}/edit`;
+                        let templateModificar = `<a href="${urlModificar}" title="Modificar"><i class='color-texto-cbre fas fa-pencil fa-fw'></i></a>`;
+                        let templateEliminar = `<a href="" title="Eliminar" id="eliminarPerfilEnlace" data-id="${idCertificacion}"><i class='fas fa-trash-can fa-fw pointer-none color-texto-cbre'></i></a>`;
+    
+                        const enlaceModificar = $('<a />').append(templateModificar).appendTo(container);
+                        const enlaceEliminar = $('<a />').append(templateEliminar).appendTo(container);
+                        
+                        enlaceEliminar.click(function (event) {
+                            event.preventDefault();
+                            
+                            Swal.fire({
+                                title: '¿Deseas continuar?',
+                                text: "¡No podrás revertir esto!",
+                                icon: 'warning',
+                                showCancelButton: true,
+                                confirmButtonColor: '#3085d6',
+                                cancelButtonColor: '#d33',
+                                confirmButtonText: 'Sí, eliminar',
+                                cancelButtonText: 'Cancelar',
+                            }).then((result) => {
+                                if (result.isConfirmed) {
+                                    url = `/admin/certificaciones/${idCertificacion}`;
+                                    const token = document.querySelector("input[name='_token']").value;
+
+                                    fetch(url, {
+                                        method: 'DELETE',
+                                        headers: {
+                                            'X-CSRF-TOKEN': token
+                                        }
+                                    })
+                                    .then(response => response.json())
+                                    .then(function (response) {
+                                        if ( response.status == 'error' ) {
+                                            Swal.fire({
+                                                icon: 'error',
+                                                title: 'Un momento...',
+                                                text: response.message
+                                            })
+                            
+                                            return;
+                                        }
+
+                                        Swal.fire(
+                                            '¡Listo!',
+                                            'La certificación ha sido eliminada.',
+                                            'success'
+                                        )
+                                    })
+                                    .then(() => cargarNoticias())
+                                    .catch(error => {
+                                        Swal.fire({
+                                            icon: 'error',
+                                            title: 'Un momento...',
+                                            text: error.message
+                                        });
+                                    });
+                                }
+                            })
+                        });
+                    }
+                }
+            ],
+            paging: {
+                pageSize: 10
+            },
+            pager: {
+                showNavigationButtons: true,
+                visible: true,
+                showPageSizeSelector: true,
+                allowedPageSizes: [5, 10, 15, 20]
+            }
+        });
+    }
+
+    function sendRequest(url, method, data) {
+        let d = $.Deferred();
+        method = method || "GET";
+        $.ajax(url, {
+            method: method || "GET",
+            data: data,
+            cache: false,
+            xhrFields: {
+                withCredentials: true
+            }
+        }).done(function(result) {
+            d.resolve(result);
+        }).fail(function(xhr) {
+            d.reject(xhr.responseJSON ? xhr.responseJSON.Message : xhr.statusText);
+        });
+
+        return d.promise();
+    }
+});
