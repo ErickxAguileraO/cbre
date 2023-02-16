09709f3a992204f3d31dc7348047f714aaa8887e
Alfonso Castillo <acastillo@aeurus.cl>
2023-02-16 19:31:51 -0300

    archivo seleccionado

--- public/js/admin/sistema/quienes_somos/edit.js
+++ public/js/admin/sistema/quienes_somos/edit.js
@@ -149,3 +149,11 @@ function setValidationMessages(response) {
     });
 });
 
+const inputFiles = document.querySelectorAll('.input-file');
+
+Array.from(inputFiles).forEach(function (inputFile) {
+    inputFile.addEventListener('change', function () {
+        const spanArchivoSeleccionado = document.querySelector('.archivo-seleccionado > span');
+        spanArchivoSeleccionado.innerHTML = inputFile.files[0].name;
+    });
+});
