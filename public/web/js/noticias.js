233a0e0a34227aa13871465a69f45b3cba8241e0
Alfonso Castillo <acastillo@aeurus.cl>
2023-02-22 10:30:05 -0300

    optimización y bugs

--- public/web/js/noticias.js
+++ public/web/js/noticias.js
@@ -1,8 +1,8 @@
-var start = 1; //establece con cuántos objetos se inicializará
-var take = 1; //establece cuántos objetos se obtendrán con cada nueva petición
+var start = 6; //establece con cuántos objetos se inicializará, y dará paso posteriormente a cuántos objetos se saltará (skip)
+var take = 6; //establece cuántos objetos se obtendrán con cada nueva petición
 var noticias = []; //array para almacenar los objetos
-let stop = false; //detiente las peticiones al cambiar su estado a true
-let wait = false; //soluciona error al hacer scroll spam
+var stop = false; //detiente las peticiones al cambiar su estado a true
+var isLoading = false; //soluciona error al hacer scroll spam
 const noticiasContainer = document.getElementById("lista-noticias"); //contenedor principal para imprimir los elementos
 
 document.addEventListener("DOMContentLoaded", async function () {
@@ -19,34 +19,35 @@ async function getInitialNoticias() {
 
 async function getNextNoticias() {
     window.onscroll = () => {
-      let bottomOfWindow = document.documentElement.scrollTop + window.innerHeight === document.documentElement.offsetHeight;
-      if (bottomOfWindow) {
-        if (!stop && !wait) {
-            isLoading(true);
+      let bottomOfnoticiasContainer = window.innerHeight + window.pageYOffset >= noticiasContainer.offsetTop + noticiasContainer.offsetHeight;
+      if (bottomOfnoticiasContainer && !stop && !isLoading) {
+            isLoadingSniper(true);
           setTimeout(async () => {
             const response = await fetch(`noticias/get/list?skip=${start}&take=${take}`);
             const data = await response.json();
             noticias.push(...data.noticias);
-            start++;
+            start = start + take;
             if (data.noticias.length === 0) {
               stop = true;
             }
+            isLoadingSniper(false);
             printNoticias();
-            isLoading(false);
-          }, Math.floor(Math.random() * (800 - 300 + 1) + 300))
-        }
+          }, Math.floor(Math.random() * (1100 - 300 + 1) + 300))
       }
     }
   }
 
 function printNoticias() {
-    noticiasContainer.innerHTML = "";
+    while (noticiasContainer.firstChild) {
+        noticiasContainer.removeChild(noticiasContainer.firstChild);
+    }
     noticias.forEach(function (noticia, i) {
-    noticiasContainer.innerHTML +=  `
+    const noticiaHTML =  `
     <div  class="noticia-home-n">
-        <a href="#">
+        <a href="noticias/${noticia.not_id}-${noticia.not_titulo.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "").replace(/[^\w ]+/g,'').replace(/ +/g,'-')
+    }">
             <div class="">
-            <img src="${noticia.urlImagen}" style="width: 100%;height: 250px;" alt="">
+            <img src="${noticia.urlImagen}" class="imagen-noticias" alt="">
             </div>
             <div class="contenido-noticia-n">
                 <div class="date-noticia">
@@ -54,7 +55,8 @@ function printNoticias() {
                     <p>Publicado el ${noticia.created_at}</p>
                 </div>
                 <h2>${noticia.not_titulo}</h2>
-                <a href="/noticias-detalle" class="ver-mas">
+                <a href="noticias/${noticia.not_id}-${noticia.not_titulo.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "").replace(/[^\w ]+/g,'').replace(/ +/g,'-')
+            }" class="ver-mas">
                     <img src="public/web/imagenes/i-linea.svg" alt="">
                     <p>Ver noticia</p>
                 </a>
@@ -62,16 +64,12 @@ function printNoticias() {
         </a>
     </div>
     `;
+    noticiasContainer.insertAdjacentHTML('beforeend', noticiaHTML);
 });
 }
 
-function isLoading(isLoading){
-    if(isLoading){
-        wait = true;
-        document.getElementById("spinner").style.display = "block";
-    }else{
-        wait = false;
-        document.getElementById("spinner").style.display = "none";
-    }
+function isLoadingSniper(status){
+    isLoading = status;
+    document.getElementById("spinner").style.display = isLoading ? "block" : "none";
 }
 
