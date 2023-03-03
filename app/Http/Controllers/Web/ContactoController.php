a22fa51e1e1e21360b05f214a313b138394f05a4
Alfonso Castillo <acastillo@aeurus.cl>
2023-03-03 17:50:43 -0300

    email de confirmación de recepción de formulario

--- app/Http/Controllers/Web/ContactoController.php
+++ app/Http/Controllers/Web/ContactoController.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Mail;
 use App\Http\Requests\Web\Contacto\StoreContactoRequest;
+use App\Mail\ContactoComprobante;
 use App\Mail\ContactoEncargado;
 use App\Models\DatoGeneral;
 
@@ -31,8 +32,9 @@ class ContactoController extends Controller
             ]);
             $datos_generales = DatoGeneral::first();
             Mail::to($datos_generales->dag_email_encargado)->send(new ContactoEncargado($request, $datos_generales));
+            Mail::to($request->email)->send(new ContactoComprobante($request, $datos_generales));
             DB::commit();
-            return response()->json(['success' => '¡Formulario completado con éxito! ¡Gracias por contactarnos! Pronto nos comunicaremos contigo.'], 200);
+            return response()->json(['success' => '¡Formulario completado con éxito! </br> ¡Gracias por contactarnos! Pronto nos comunicaremos contigo.'], 200);
         } catch (\Throwable $th) {
             DB::rollback();
             return response()->json(['error' => $th->getMessage()], 401);
