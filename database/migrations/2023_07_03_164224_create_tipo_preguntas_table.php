6f77370c9ace04e558fc20ae01f6811eb0a67440
Alfonso Castillo <acastillo@aeurus.cl>
2023-07-03 18:20:07 -0400

    migraciones tipo preguntas

--- database/migrations/2023_07_03_164224_create_tipo_preguntas_table.php
+++ database/migrations/2023_07_03_164224_create_tipo_preguntas_table.php
@@ -1,8 +1,9 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 class CreateTipoPreguntasTable extends Migration
 {
@@ -18,8 +19,23 @@ class CreateTipoPreguntasTable extends Migration
             $table->string('tipp_nombre');
             $table->timestamps();
         });
+
+        $this->run();
     }
 
+    private function run()
+    {
+        $tipoPreguntas = [
+            ['tipp_nombre' => 'Selección individual', 'created_at' => now(), 'updated_at' => now()],
+            ['tipp_nombre' => 'Selección múltiple', 'created_at' => now(), 'updated_at' => now()],
+            ['tipp_nombre' => 'Párrafo', 'created_at' => now(), 'updated_at' => now()],
+            ['tipp_nombre' => 'HSE - Accidentabilidad', 'created_at' => now(), 'updated_at' => now()]
+        ];
+
+        DB::transaction(function () use ($tipoPreguntas) {
+            DB::table('tipo_preguntas')->insert($tipoPreguntas);
+        });
+    }
     /**
      * Reverse the migrations.
      *
