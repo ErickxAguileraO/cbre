af40a202a0aa37fe0493fdf8beb725fb1035f8d5
Alfonso Castillo <acastillo@aeurus.cl>
2023-07-08 13:12:55 -0400

    relaciones de modelos actualizada

--- app/Models/Pregunta.php
+++ app/Models/Pregunta.php
@@ -8,4 +8,29 @@ use Illuminate\Database\Eloquent\Model;
 class Pregunta extends Model
 {
     use HasFactory;
+
+    protected $table = 'preguntas';
+
+    protected $primaryKey = 'pre_id';
+
+    protected $fillable = [
+        'pre_pregunta',
+        'pre_obligatorio',
+    ];
+
+    public function formulario()
+    {
+        return $this->belongsTo(Formulario::class, 'pre_formulario_id', 'form_id');
+    }
+
+    public function tipoPregunta()
+    {
+        return $this->belongsTo(TipoPregunta::class, 'pre_tipo_pregunta_id', 'tipp_id');
+    }
+
+    public function opciones()
+    {
+        return $this->hasMany(Opcion::class, 'opc_pregunta_id', 'pre_id');
+    }
+
 }
