e7237d9b08fbb244b8daf92890eb4fd9db61338c
Alfonso Castillo <acastillo@aeurus.cl>
2023-07-23 20:42:26 -0400

    estructura del blade corregida

--- resources/views/admin/formulario_area_tecnica/edit.blade.php
+++ resources/views/admin/formulario_area_tecnica/edit.blade.php
@@ -2,121 +2,85 @@
 @section('title', 'Editar formulario')
 @section('content')
     @push('stylesheets')
-      <link rel="stylesheet" href="{{ asset('/public/css/componentes/tab/tab.css') }}">
-      <link rel="stylesheet" href="{{ asset('/public/css/componentes/modal/modal.css') }}">
+        <link rel="stylesheet" href="{{ asset('/public/css/componentes/tab/tab.css') }}">
+        <link rel="stylesheet" href="{{ asset('/public/css/componentes/modal/modal.css') }}">
 
         <style>
-            body{
+            body {
                 background: #F2F2F2;
             }
-            main[role=main]{
+
+            main[role=main] {
                 background: #F2F2F2;
             }
-            .form-group{
+
+            .form-group {
                 margin: 0px;
             }
         </style>
-
     @endpush
-    <div  id="" class="formulario nuevo-formulario">
 
-        <livewire:administracion.formulario.post-form :formId="$formulario->form_id"/>
+    <div id="" class="formulario nuevo-formulario">
+
+        <livewire:administracion.formulario.post-form :formId="$formulario->form_id" />
 
         <a href="{{ route('formulario-area-tecnica.index') }}" class="row row-responsive link-atras">
             <i class="far fa-arrow-left"></i>
             Volver al listado
         </a>
-        <div class="tab">
-            <ul class="tab__lista-botones">
-                <li class="tab__boton" onclick="mostrarTab(event, 'tab1')" style="border-radius: 8px 0px 0px 8px;">
-                    <p class="tab__nombre">Formulario</p>
-                </li>
 
-                <li class="tab__boton" onclick="mostrarTab(event, 'tab2')" style="border-radius: 0px 8px 8px 0px;">
-                    <p class="tab__nombre">Historial</p>
-                </li>
-            </ul>
+        <div class="grid-header-2">
+            <h1 class="col-xl">Formulario equipo de limpieza</h1>
+            <div class="row datos-formulario">
+                <p class="margin-top-5">Estado</p>
+                <div class="estado-formulario">Borrador</div>
+                <div class="form-group">
+                    <div class="select-manual" class="">
+                        <p>Opciones</p>
+                        <i class="fas fa-sort-down color-texto-cbre menos-top"></i>
 
-            <div id="tab1" class="tab__contenido">
-                <div class="grid-header-2">
-                    <h1 class="col-xl">Formulario equipo de limpieza</h1>
-                    <div class="row datos-formulario">
-                        <p class="margin-top-5">Estado</p>
-                        <div class="estado-formulario">Borrador</div>
-                        <div class="form-group">
-                            <div class="select-manual" class="">
-                                <p>Opciones</p>
-                                <i class="fas fa-sort-down color-texto-cbre menos-top"></i>
+                        <div class="option-select-manual">
 
-                                <div class="option-select-manual">
+                            <div class="row-option modalPublicar__abrirBtn"><i class="fas fa-paper-plane"></i> Publicar y
+                                enviar</div>
+                            <a href="{{ route('formulario-area-tecnica.ver.formulario', $formulario->form_id) }}"
+                                target="_blank" class="row-option"><i class="fas fa-eye"></i> Visualizar</a>
+                            <div id="eliminar-formulario" class="row-option"><i class="fas fa-trash-alt"></i> Eliminar</div>
+                            <div class="row-option modalObservacion__abrirBtn"><i class="fas fa-edit"></i> Observación</div>
 
-                                    <div class="row-option modalPublicar__abrirBtn"><i class="fas fa-paper-plane"></i> Publicar y enviar</div>
-                                    <a href="{{ route('formulario-area-tecnica.ver.formulario', $formulario->form_id) }}" target="_blank" class="row-option"><i class="fas fa-eye"></i> Visualizar</a>
-                                    <div id="eliminar-formulario" class="row-option"><i class="fas fa-trash-alt"></i> Eliminar</div>
-                                    <div class="row-option modalObservacion__abrirBtn"><i class="fas fa-edit"></i> Observación</div>
-
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="contenedor-form-preguntas">
-                    <livewire:administracion.formulario.edit-form :formId="$formulario->form_id"/>
-                </div>
-
-                <div class="linea-separadora"></div>
-                <div class="botones-formulario">
-                    <a id="eliminar-formulario" class="modalFile__cerrarBtn modalFile__btnN modalFile__botonSecundario text-dark text-decoration-none">Eliminar</a>
-                    <input type="hidden" id="form_id" name="form_id" value="{{ $formulario->form_id }}">
-                    <a href="{{ route('formulario-area-tecnica.index') }}" class="modalFile__btnN modalFile__botonPrimario text-white text-decoration-none">Dejar como borrador</a>
-                </div>
-
-            </div>
-
-            <div id="tab2" class="tab__contenido">
-                <h1 class="col-xl">Historial formulario</h1>
-                <div class="div-formulario-n">
-                    <div class="historial">
-                        <div class="historial__linea">
-                            <div class="historial__lineaCirculo"></div>
-                        </div>
-                        <div class="historial__datos sin-margen">
-                            <p>21 Nov 2023</p>
-                            <h3>Creación de formulario</h3>
-                            <p>Estado: Borrador</p>
-                            <p>Creado por Benjamín Arias</p>
-                        </div>
-                    </div>
-
-                    <div class="historial">
-                        <div class="historial__linea">
-                            <div class="historial__lineaCirculo"></div>
-                        </div>
-                        <div class="historial__datos sin-margen">
-                            <p>21 Nov 2023</p>
-                            <h3>Creación de formulario</h3>
-                            <p>Estado: Borrador</p>
-                            <p>Creado por Benjamín Arias</p>
-                        </div>
-                    </div>
-
-                    <div class="historial">
-                        <div class="historial__linea">
-                            <div class="historial__lineaCirculo"></div>
-                        </div>
-                        <div class="historial__datos sin-margen">
-                            <p>21 Nov 2023</p>
-                            <h3>Creación de formulario</h3>
-                            <p>Estado: Borrador</p>
-                            <p>Creado por Benjamín Arias</p>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
 
+        <div class="contenedor-form-preguntas">
+            <livewire:administracion.formulario.edit-form :formId="$formulario->form_id" />
+        </div>
+
+        <div class="linea-separadora"></div>
+        <div class="botones-formulario">
+            <a id="eliminar-formulario"
+                class="modalFile__cerrarBtn modalFile__btnN modalFile__botonSecundario text-dark text-decoration-none">Eliminar</a>
+            <input type="hidden" id="form_id" name="form_id" value="{{ $formulario->form_id }}">
+
+            <div class="form-group">
+                <button id="guardar" type="submit" class="modalFile__btnN modalFile__botonPrimario text-white text-decoration-none" value="Guardar"
+                    type="button">
+                    <div id="default" class="d-block">
+                        <span>Guardar como borrador</span>
+                        <i class="fas fa-save ml-2 mr-2"></i>
+                    </div>
+                    <div id="loading" class="d-none">
+                        <span>Guardar como borrador</span>
+                        <span class="spinner-border spinner-border-md ml-1" role="status" aria-hidden="true"></span>
+                    </div>
+                </button>
+            </div>
+
+        </div>
+
+    </div>
 
 @endsection
 
@@ -124,24 +88,25 @@
     <script>
         // Opciones
         $(".option-select-manual").hide();
-        $(".select-manual").click(function () {
+        $(".select-manual").click(function() {
             $(".option-select-manual").toggle();
         })
 
         // Modal publicar
-        $(".modalPublicar__abrirBtn").on('click', function () {
+        $(".modalPublicar__abrirBtn").on('click', function() {
             $(".contenedor__modalPublicar").css("display", "flex");
             $('#mySelect').val('').trigger('change');
             $('#mySelect').select2();
         });
 
-        $(".modalPublicar__cerrarBtn").on('click', function () {
+        $(".modalPublicar__cerrarBtn").on('click', function() {
             $(".contenedor__modalPublicar").css("display", "none");
         });
     </script>
-        <script src="{{ asset('/public\js\admin\sistema\area_tecnica\publicar_formulario.js') }}"></script>
+    <script src="{{ asset('/public\js\admin\sistema\area_tecnica\borrador_formulario.js') }}"></script>
+    <script src="{{ asset('/public\js\admin\sistema\area_tecnica\publicar_formulario.js') }}"></script>
     <script src="{{ asset('/public\js\admin\sistema\area_tecnica\eliminar_formulario.js') }}"></script>
     <script src="{{ asset('/public/css/componentes/tab/tab.js') }}"></script>
-{{--     <script src="{{ asset('/public/css/componentes/modal/modal.js') }}"></script>
+    {{--     <script src="{{ asset('/public/css/componentes/modal/modal.js') }}"></script>
     <script src="{{ asset('/public/js/script.js') }}"></script> --}}
 @endpush
